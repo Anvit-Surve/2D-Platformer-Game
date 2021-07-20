c7999ef1817da087d220cb54ba2f4fdff003ce1f
Anvit Surve <anvit02surve@gmail.com>
2021-07-20 20:13:54 +0530

    Added Crouch animation and jump using vertical

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,28 +5,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    private void Awake()
-    {
-        Debug.Log("Player Controller Awake");
-    }
+    //private void Awake()
+    //{
+    //    Debug.Log("Player Controller Awake");
+    //}
 
     //private void OnCollisionEnter2D(Collision2D collision)
     //{
     //    Debug.Log("Collision: " + collision.gameObject.name);
     //}
 
+
     private void Update()
     {
+        //Run Script
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
-        } 
-        else if (speed > 0){
+        }
+        else if (speed > 0)
+        {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        //Crouch Script
+        bool Crouch = Input.GetKey(KeyCode.LeftControl);
+        if (Crouch)
+        {
+            animator.SetBool("Crouch", true);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+        }
+
+        //Jump Script
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        //bool Jump = Input.GetKey("space");
+        //if(Jump)
+        //{
+        //    animator.SetBool("Jump", true);
+        //}
     }
 }
