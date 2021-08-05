64819ae810ffcf3e935b5037e5ac864457890912
Anvit Surve <anvit02surve@gmail.com>
2021-08-06 01:03:36 +0530

    Level2 Design

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,6 +13,7 @@ public class EnemyController : MonoBehaviour
         if (collision.gameObject.GetComponent<PlayerController>())
         {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.JumpAnimationFalse();
             playerController.HurtPlayer();
         }
     }
@@ -20,7 +21,7 @@ public class EnemyController : MonoBehaviour
     private void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 1f);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 0.75f);
         if (groundInfo.collider == false)
         {
             if (movingRight)
